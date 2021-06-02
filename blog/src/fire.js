5b81264b141655539bf163f1b124e3bcc1c5e2dc
sahilsingh21 <ss3842486@gmail.com>
2021-06-02 23:11:02 +0530

    Creating login page with react

---
+++ blog/src/fire.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBjsChhd1qk6rI6uX4bkCuRQWhUbAS_e20",
+    authDomain: "login-e9b4e.firebaseapp.com",
+    projectId: "login-e9b4e",
+    storageBucket: "login-e9b4e.appspot.com",
+    messagingSenderId: "222955948190",
+    appId: "1:222955948190:web:6ce266813881879ef6938f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
