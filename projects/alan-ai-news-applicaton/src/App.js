e280a44fc0820fdbe724f68746d3461d6de23c4d
sahilsingh21 <ss3842486@gmail.com>
2021-07-23 17:02:18 +0530

    news voice assistent

--- projects/alan-ai-news-applicaton/src/App.js
+++ projects/alan-ai-news-applicaton/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+// import { command } from 'yargs';
 
-function App() {
+const alanKey = '57e952c6341522f760e5370876780ac12e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles }) => {
+                if(command === 'newHeadlines'){
+                    console.log(articles);
+                }
+            }
+        })
+    }, [])
     return (
         <div>
-            Alan Ai
+            Alan Ai News Application
         </div>
     );
 }
 
-export default App
+export default App;
